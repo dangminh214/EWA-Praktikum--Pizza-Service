bf94e2900b091437a06b62cff38e7947eccff082
dangminh214 <dangminh214@gmail.com>
2023-05-21 05:46:00 +0200

    KundeStatus Fix: show Warning if there is any lastest Orders

--- src/Praktikum/Prak3/KundenStatus.php
+++ src/Praktikum/Prak3/KundenStatus.php
@@ -14,42 +14,46 @@ class Kunde extends Page
     }
     protected function getViewData():array
     {
+        $ordering_List = array();
         session_start();
-        $last_ordering_id = $_SESSION['last_ordering_id'];
-        $sqlCommandToGroupUpOrderingID = "SELECT ordering_id, 
+        if ($_SESSION) {
+            $last_ordering_id = $_SESSION['last_ordering_id'];
+            $sqlCommandToGroupUpOrderingID = "SELECT ordering_id, 
        GROUP_CONCAT(article_id) AS article_ids, GROUP_CONCAT(status) AS status_ids
         FROM ordered_article
         WHERE ordering_id = $last_ordering_id
         GROUP BY ordering_id;";
 
-        $recordSet = $this->database->query($sqlCommandToGroupUpOrderingID);
-        if (!$recordSet) {
-            throw new Exception("keine Article in der Datenbank");
+            $recordSet = $this->database->query($sqlCommandToGroupUpOrderingID);
+            if (!$recordSet) {
+                throw new Exception("keine Article in der Datenbank");
+            }
+
+
+            while ($record = $recordSet->fetch_assoc()) {
+                $get_ordering_id_from_datenbank = $record["ordering_id"];
+                $get_article_id_from_datenbank = $record["article_ids"];
+                $get_article_status_from_datenbank = $record["status_ids"];
+
+                $ordering_List[] = array(
+                    "ordering_id" => $get_ordering_id_from_datenbank,
+                    "article_id" => $get_article_id_from_datenbank,
+                    "article_status" => $get_article_status_from_datenbank
+                );
+            }
+            $recordSet->free();
         }
 
-        $ordering_List = array();
-        while ($record = $recordSet->fetch_assoc()) {
-            $get_ordering_id_from_datenbank = $record["ordering_id"];
-            $get_article_id_from_datenbank = $record["article_ids"];
-            $get_article_status_from_datenbank = $record["status_ids"];
-
-            $ordering_List[] = array(
-                "ordering_id" => $get_ordering_id_from_datenbank,
-                "article_id" => $get_article_id_from_datenbank,
-                "article_status" => $get_article_status_from_datenbank
-            );
-
-        }
-        $recordSet->free();
         return $ordering_List;
     }
 
     //Generate JSON File
     protected function generateView():void
     {
-        $list = $this->getViewData();
-        $jsonString = json_encode($list);
-        echo $jsonString;
+            $list = $this->getViewData();
+            $jsonString = json_encode($list);
+            echo $jsonString;
+
     }
 
     protected function processReceivedData():void
