1d82377e746fcd9112de224a79b817f6892272b8
dangminh214 <dangminh214@gmail.com>
2023-06-01 09:11:27 +0200

    Update index.php

--- src/index.php
+++ src/index.php
@@ -3,12 +3,7 @@ header("Content-type: text/html");
 
 $Prak="";
 if (is_dir("Praktikum")) {
-    $Prak = "<p>Your lab exercises are located in the folder <a href=\"./Praktikum\">Praktikum.</a></p>";
-}
-
-$Exam="";
-if (is_dir("Exam")) {
-    $Exam = "    <p><strong>Use the folder <a href=\"./Exam\">Exam</a> (located in src/Exam) for your solution of the examination. Do not use any other folder!</strong></p>";
+    $Prak = "<p>My Project: <a href=\"./Praktikum\">Praktikum.</a></p>";
 }
 
 echo <<<EOT
@@ -20,14 +15,10 @@ echo <<<EOT
 </head>
 <body>
 <header>
-    <h1>EWA using Docker-Containers</h1>
+    <h1>Project: Pizza Service Website</h1>
 </header>
 <main>
-    <p>This is the content of the file 'index.php' in the folder 'src'!</p>
-    <p>Put your own code into that folder or its subfolders!</p>
-    $Exam
-    <p>For working examples you may have a look at <a href="./Klausuren">recent EWA-examinations!</a></p>
-    <p>The examples from the lecture are located in the folder <a href="./Demos">Demos.</a></p>
+    Link to website: 
     $Prak
 </main>
 </body>
