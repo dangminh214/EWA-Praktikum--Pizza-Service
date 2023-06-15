3dadb6817ad2fa931dc92f63ff37c642c397294b
dangminh214 <dangminh214@gmail.com>
2023-06-15 10:05:16 +0200

    JS GUI for Bestellung.php

--- src/Praktikum/Prak4/Bestellung.js
+++ src/Praktikum/Prak4/Bestellung.js
@@ -6,7 +6,7 @@ function calculateToTalPrice() {
     var warenkorb = document.getElementById("warenkorb");
     if (warenkorb.options.length > 0) {
         for (let i = 0; i < warenkorb.options.length; i++) {
-            var string_price = warenkorb.options[i].value;
+            var string_price = warenkorb.options[i].id;
             var float_price = parseFloat(string_price);
             total_price = (total_price + float_price);
             fixed_total_price = total_price.toFixed(2);
@@ -19,56 +19,56 @@ function calculateToTalPrice() {
 }
 
 setInterval(calculateToTalPrice, 1000);
- function clickFunction(event) {
-     //access to name and price of articel using DOM
-     let article_name = event.target.classList.item(0);
-     let string_price = document.getElementById(`${article_name}.price`).textContent;
+function clickFunction(event) {
+    //access to name and price of articel using DOM
+    let article_name = event.target.classList.item(0);
+    let string_price = document.getElementById(`${article_name}.price`).textContent;
 
-     let float_price = parseFloat(string_price);
+    let float_price = parseFloat(string_price);
 
-     //create new option in the Warenkorb
-     let newOption = document.createElement("option");
-     let article_id;
+    //create new option in the Warenkorb
+    let newOption = document.createElement("option");
+    let article_id;
 
-     //generate article_id using article_name;
-     switch (article_name) {
-         case "Salami":
-             article_id = 1;
-             newOption.id = article_id;
-             newOption.value = float_price;
-             var textNode = document.createTextNode(`${article_name}`);
-             newOption.appendChild(textNode);
-             var warenkorb = document.getElementById("warenkorb");
-             warenkorb.appendChild(newOption);
-             break;
+    //generate article_id using article_name;
+    switch (article_name) {
+        case "Salami":
+            article_id = 1;
+            newOption.id = float_price;
+            newOption.value = article_id;
+            var textNode = document.createTextNode(`${article_name}`);
+            newOption.appendChild(textNode);
+            var warenkorb = document.getElementById("warenkorb");
+            warenkorb.appendChild(newOption);
+            break;
 
-         case "Vegetaria":
-             article_id = 2;
-             newOption.id = article_id;
-             newOption.value = float_price;
-             var textNode = document.createTextNode(`${article_name}`);
-             newOption.appendChild(textNode);
-             var warenkorb = document.getElementById("warenkorb");
-             warenkorb.appendChild(newOption);
-             break;
+        case "Vegetaria":
+            article_id = 2;
+            newOption.id = float_price;
+            newOption.value = article_id;
+            var textNode = document.createTextNode(`${article_name}`);
+            newOption.appendChild(textNode);
+            var warenkorb = document.getElementById("warenkorb");
+            warenkorb.appendChild(newOption);
+            break;
 
-         case "Spinat-Hühnchen":
-             article_id = 3;
-             newOption.id = article_id;
-             newOption.value = float_price;
-             var textNode = document.createTextNode(`${article_name}`);
-             newOption.appendChild(textNode);
-             var warenkorb = document.getElementById("warenkorb");
-             warenkorb.appendChild(newOption);
-             break;
-     }
+        case "Spinat-Hühnchen":
+            article_id = 3;
+            newOption.id = float_price;
+            newOption.value = article_id;
+            var textNode = document.createTextNode(`${article_name}`);
+            newOption.appendChild(textNode);
+            var warenkorb = document.getElementById("warenkorb");
+            warenkorb.appendChild(newOption);
+            break;
+    }
 }
 
 function deleteAllOptions() {
-     let warenkorb = document.getElementById("warenkorb");
-     while (warenkorb.options.length > 0) {
-         warenkorb.remove(0);
-     }
+    let warenkorb = document.getElementById("warenkorb");
+    while (warenkorb.options.length > 0) {
+        warenkorb.remove(0);
+    }
 }
 
 function deleteSelectedOption() {
@@ -80,7 +80,6 @@ function deleteSelectedOption() {
         }
     }
 }
-
 
 //TODO: FIX select_all_items()
 function select_all_items() {
